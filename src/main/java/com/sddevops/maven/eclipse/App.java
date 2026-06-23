173a3c37598a61db04b957568cbbf353bf360169
zinkodev <network.zinkothu@gmail.com>
2026-06-23 21:09:17 +0800

    Change Second Line

--- src/main/java/com/sddevops/maven/eclipse/App.java
+++ src/main/java/com/sddevops/maven/eclipse/App.java
@@ -4,6 +4,6 @@ package com.sddevops.maven.eclipse;
 public class App {
     public static void main(String[] args) {
         System.out.println("Git Test!");
-        System.out.println("new line!");
+        System.out.println("change line!");
     }
 }
