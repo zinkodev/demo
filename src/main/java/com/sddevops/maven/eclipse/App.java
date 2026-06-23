e158efc5c140cad83abed66b87f7b972df1172cd
zinkodev <network.zinkothu@gmail.com>
2026-06-23 20:52:58 +0800

    (Feat) Add test line

--- src/main/java/com/sddevops/maven/eclipse/App.java
+++ src/main/java/com/sddevops/maven/eclipse/App.java
@@ -1,10 +1,9 @@
 package com.sddevops.maven.eclipse;
 
-/**
- * Hello world!
- */
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Git Test!");
+        System.out.println("new line!");
     }
 }
