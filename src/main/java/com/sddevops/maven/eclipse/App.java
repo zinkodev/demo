a50935908e0fea8f3316ff264edd97fc75279c6d
zinkodev <network.zinkothu@gmail.com>
2026-06-25 20:20:02 +0800

    Feat : Add Introductory Line to test commit and push feature

--- src/main/java/com/sddevops/maven/eclipse/App.java
+++ src/main/java/com/sddevops/maven/eclipse/App.java
@@ -5,5 +5,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Git Test!");
         System.out.println("change line!");
+        System.out.println("change Second line!");
     }
 }
